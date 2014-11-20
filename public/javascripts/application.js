b0c73cfce250e33d6624280f9ce9bef44842a15a
Thiyagarajan Veluchamy <thiyagarajan@moorem.com>
2014-11-20 16:43:57 +0530

    all js added

---
+++ public/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require bootstrap.min
+//= require grids.min
+//= require jquery.flexslider-min
+//= require jquery.slicknav.min
+//= require jquery.hoverintent
+//= require dropnav
+//= require jquery.colorbox-min
