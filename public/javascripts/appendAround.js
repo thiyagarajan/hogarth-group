74bbaf1b7826e74a083459f36214588af49f0af5
Thiyagarajan Veluchamy <thiyagarajan@moorem.com>
2015-03-26 18:33:23 +0530

    pulled latest production

--- public/javascripts/appendAround.js
+++ public/javascripts/appendAround.js
@@ -5,38 +5,4 @@ how-to:
 	3. Place your appendAround content in one of the potential containers
 	4. Call appendAround() on that element when the DOM is ready
 */
-(function( $ ){
-	$.fn.appendAround = function(){
-	  return this.each(function(){
-      
-	    var $self = $( this ),
-	        att = "data-set",
-	        $parent = $self.parent(), 
-	        parent = $parent[ 0 ],
-	        attval = $parent.attr( att ),
-	        $set = $( "["+ att +"='" + attval + "']" );
-
-		function isHidden( elem ){
-			return $(elem).css( "display" ) === "none";
-		}
-
-		function appendToVisibleContainer(){
-			if( isHidden( parent ) ){
-				var found = 0;
-				$set.each(function(){
-					if( !isHidden( this ) && !found ){
-						$self.appendTo( this );
-						found++;
-						parent = this;
-					}
-				});
-	      	}
-	    }
-      
-	    appendToVisibleContainer();
-      
-	    $(window).bind( "resize", appendToVisibleContainer );
-      
-	  });
-	};
-}( jQuery ));
+(function($){$.fn.appendAround=function(){return this.each(function(){var $self=$(this),att="data-set",$parent=$self.parent(),parent=$parent[0],attval=$parent.attr(att),$set=$("["+att+"='"+attval+"']");function isHidden(elem){return $(elem).css("display")==="none"}function appendToVisibleContainer(){if(isHidden(parent)){var found=0;$set.each(function(){if(!isHidden(this)&&!found){$self.appendTo(this);found++;parent=this}})}}appendToVisibleContainer();$(window).bind("resize",appendToVisibleContainer)})}}(jQuery));
