7cccb7f2e67066d21a538c7b605fe6a3f4513a9c
Thiyagarajan Veluchamy <thiyagarajan@moorem.com>
2015-03-31 15:48:05 +0530

    updated

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -28,3 +28,13 @@ how-to:
 	4. Call appendAround() on that element when the DOM is ready
 */
 (function($){$.fn.appendAround=function(){return this.each(function(){var $self=$(this),att="data-set",$parent=$self.parent(),parent=$parent[0],attval=$parent.attr(att),$set=$("["+att+"='"+attval+"']");function isHidden(elem){return $(elem).css("display")==="none"}function appendToVisibleContainer(){if(isHidden(parent)){var found=0;$set.each(function(){if(!isHidden(this)&&!found){$self.appendTo(this);found++;parent=this}})}}appendToVisibleContainer();$(window).bind("resize",appendToVisibleContainer)})}}(jQuery));
+  
+ $(function(){
+      $('.selectpicker').bind('change', function () {
+         var url = "/timetables/" + $(this).val();
+          if (url) {
+              window.location.replace(url);
+          }
+          return false;
+      });
+    }); 
